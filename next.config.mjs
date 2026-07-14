e51f05094354bfd100e985327afdb92e47d0a24b
victus-owner <ashishprajapati0202@gmail.com>
2026-07-15 00:44:36 +0530

    memory less

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Structural production check errors override karne ke liye safe hai
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Serverless functions compilation deployment speed badhane ke liye safe hai
-    ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
+  },
+  turbopack: {
+    root: process.cwd(),
   },
 };
 
